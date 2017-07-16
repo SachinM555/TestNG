56fa31e67a5acf945dfeb031087ad4a4e3675141
seleniumwdjj@gmail.com <sachin.qa4it@gmail.com>
2017-07-16 12:39:06 +0530

    Pushed to master

--- Selenium/Sspkm/src/spkm/SPK.java
+++ Selenium/Sspkm/src/spkm/SPK.java
@@ -2,6 +2,7 @@ package spkm;
 
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
@@ -11,8 +12,8 @@ public class SPK {
  
   @Test
   public void main() {
-	  System.setProperty("webdriver.gecko.driver", "D:\\Selenium\\Drivers\\geckodriver.exe");
-      driver = new FirefoxDriver();
+	  System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\Drivers\\chromedriver.exe");
+      driver = new ChromeDriver();
       driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
       driver.get("http://www.google.co.in");
       
